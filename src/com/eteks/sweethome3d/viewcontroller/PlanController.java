6c8e4df66216e0999b177205b40685c9eaa7f5fb
Dawid Laszuk <dawid.laszuk@osano.com>
2026-08-14 16:58:04 -0700

    fix(controller): drop a piece's cached sides when it is rotated
    The margin areas kept in furnitureSidesCache to snap a dragged piece
    side by side follow a piece's location and size in plan, but not its
    angle: a piece rotated in place kept snapping along its old contour
    until it also moved. Rotating changes the contour the cached area is
    built from, so the rotation has to drop the cache entry too.

--- src/com/eteks/sweethome3d/viewcontroller/PlanController.java
+++ src/com/eteks/sweethome3d/viewcontroller/PlanController.java
@@ -2505,6 +2505,7 @@ public class PlanController extends FurnitureController implements Controller {
           String propertyName = ev.getPropertyName();
           if (HomePieceOfFurniture.Property.X.name().equals(propertyName)
               || HomePieceOfFurniture.Property.Y.name().equals(propertyName)
+              || HomePieceOfFurniture.Property.ANGLE.name().equals(propertyName)
               || HomePieceOfFurniture.Property.WIDTH_IN_PLAN.name().equals(propertyName)
               || HomePieceOfFurniture.Property.DEPTH_IN_PLAN.name().equals(propertyName)) {
             furnitureSidesCache.remove((HomePieceOfFurniture)ev.getSource());
@@ -11016,7 +11017,7 @@ public class PlanController extends FurnitureController implements Controller {
       Wall previousWall = this.wallEndAtStart != null
           ? this.wallEndAtStart
           : this.wallStartAtStart;
-      // Create a new wall with an angle equal to previous wall angle - 90°
+      // Create a new wall with an angle equal to previous wall angle - 90ï¿½
       double previousWallAngle = Math.PI - Math.atan2(previousWall.getYStart() - previousWall.getYEnd(),
           previousWall.getXStart() - previousWall.getXEnd());
       previousWallAngle -=  Math.PI / 2;
@@ -12568,7 +12569,7 @@ public class PlanController extends FurnitureController implements Controller {
       float newPitch = (float)(this.oldPitch
           + (y - getYLastMousePress()) * Math.cos(this.selectedCamera.getYaw()) * Math.PI / 360
           - (x - getXLastMousePress()) * Math.sin(this.selectedCamera.getYaw()) * Math.PI / 360);
-      // Check new angle is between -90° and 90°
+      // Check new angle is between -90ï¿½ and 90ï¿½
       newPitch = Math.max(newPitch, -(float)Math.PI / 2);
       newPitch = Math.min(newPitch, (float)Math.PI / 2);
 
@@ -14273,7 +14274,7 @@ public class PlanController extends FurnitureController implements Controller {
       float [][] points = this.newRoom.getPoints();
       this.xPreviousPoint = points [points.length - 1][0];
       this.yPreviousPoint = points [points.length - 1][1];
-      // Create a new side with an angle equal to previous side angle - 90°
+      // Create a new side with an angle equal to previous side angle - 90ï¿½
       double previousSideAngle = Math.PI - Math.atan2(points [points.length - 2][1] - points [points.length - 1][1],
           points [points.length - 2][0] - points [points.length - 1][0]);
       previousSideAngle -=  Math.PI / 2;
@@ -15258,7 +15259,7 @@ public class PlanController extends FurnitureController implements Controller {
       float [][] points = this.newPolyline.getPoints();
       this.xPreviousPoint = points [points.length - 1][0];
       this.yPreviousPoint = points [points.length - 1][1];
-      // Create a new segment with an angle equal to previous segment angle - 90°
+      // Create a new segment with an angle equal to previous segment angle - 90ï¿½
       double previousSegmentAngle = Math.PI - Math.atan2(points [points.length - 2][1] - points [points.length - 1][1],
           points [points.length - 2][0] - points [points.length - 1][0]);
       previousSegmentAngle -=  Math.PI / 2;
